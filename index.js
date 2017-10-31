3081c29e9e552d06f29727d8dbdf0f12574fcf30
Omar Delarosa <thedelarosa@gmail.com>
2017-10-31 18:17:44 -0400

    0.0.2: adding support for a beforeRender function to be applied to stylus

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const stylus = require('stylus')
 
 module.exports = (css, settings) => {
+  if (!settings) settings = {};
   const cssWithPlaceholders = css
     .replace(/\:\s*%%styled-jsx-placeholder-(\d+)%%/g, (_, id) =>
       `: styled-jsx-placeholder-${id}()`
@@ -9,7 +10,13 @@ module.exports = (css, settings) => {
       `/*%%styled-jsx-placeholder-${id}%%*/`
     )
 
-  const preprocessed = stylus.render(css.toString());
+  const beforeRender = settings.beforeRender;
+  let preprocessed;
+  if (typeof beforeRender === 'function') {
+    preprocessed = beforeRender(stylus, css).render(css.toString());
+  } else {
+    preprocessed = stylus.render(css.toString());
+  }
 
   return preprocessed
     .replace(/\:\s*styled-jsx-placeholder-(\d+)\(\)/g, (_, id) =>
--- test.js
+++ test.js
@@ -1,24 +1,150 @@
 const expect = require('chai').expect;
+const sinon = require('sinon');
 const plugin = require('./')
 const stripIndent = require('strip-indent');
+const stylus = require('stylus');
 
 const cleanup = str => stripIndent(str).trim()
 
 describe('styled-jsx-plugin-stylus', () => {
-  it('converts colors', () => {
-    expect(
-      cleanup(plugin(
+  describe('basic props', () => {
+
+    const cases = [
+      [
+        'color properties',
         `
           .red
             color red
+        `,
         `
-      ))
-    ).to.eq(
-      cleanup(`
-        .red {
-          color: #f00;
+          .red {
+            color: #f00;
+          }
+        `
+      ],
+      [
+        'nested selectors',
+        `
+          .outer-class
+            .inner-class
+              color green
+              display none
+        `,
+        `
+        .outer-class .inner-class {
+          color: #008000;
+          display: none;
         }
-      `)
+        `
+      ],
+      [
+        'ampersand & operator',
+        `
+          .first
+            &-second
+              color green
+              display none
+        `,
+        `
+        .first-second {
+          color: #008000;
+          display: none;
+        }
+        `
+      ]
+    ];
+
+    cases.forEach(([description, stylus, css]) => {
+      it(description, () => {
+        expect(
+          cleanup(plugin(stylus))
+        ).to.eq(
+          cleanup(css)
+        );
+      });
+    });
+  });
+
+  describe('with settings', () => {
+    const inputStylus = cleanup(
+      `
+      .my-class
+        color red
+      `
     );
+    const outputCss = cleanup(
+      `
+      .my-class {
+        color: #f00;
+      }
+      `
+    );
+    let beforeRender = sinon.spy((stylus, css) => stylus);
+    let render = sinon.spy(stylus, 'render');
+    let stylusMock;
+    let opts = {};
+    let preprocessedCss;
+
+    describe('when beforeRender function is provided', () => {
+      beforeEach(() => {
+        render.reset();
+        beforeRender.reset();
+        opts = {
+          beforeRender
+        };
+        preprocessedCss = plugin(inputStylus, opts);
+      });
+
+      it('returns the correct preprocessed css string', () => {
+        expect(preprocessedCss.trim()).to.eq(outputCss.trim());
+      });
+
+      it('calls beforeRender function once', () => {
+        expect(beforeRender.calledOnce).to.eq(true);
+      });
+
+      it('calls beforeRender with a stylus object', () => {
+        expect(beforeRender.calledWith(stylus)).to.eq(true);
+      });
+
+      it('calls render function once', () => {
+        expect(render.calledOnce).to.eq(true);
+      });
+
+      it('calls render function with input css', () => {
+        expect(render.calledWith(inputStylus)).to.eq(true);
+      });
+    });
+
+    describe('when beforeRender function is not provided', () => {
+      beforeEach(() => {
+        render.reset();
+        beforeRender.reset();
+        opts = {
+          beforeRender: null
+        };
+        preprocessedCss = plugin(inputStylus, opts);
+      });
+
+      it('returns the correct preprocessed css string', () => {
+        expect(preprocessedCss.trim()).to.eq(outputCss.trim());
+      });
+
+      it('calls beforeRender function once', () => {
+        expect(beforeRender.calledOnce).to.eq(false);
+      });
+
+      it('calls beforeRender with a stylus object', () => {
+        expect(beforeRender.calledWith(stylus)).to.eq(false);
+      });
+
+      it('calls render function once', () => {
+        expect(render.calledOnce).to.eq(true);
+      });
+
+      it('calls render function with input css', () => {
+        expect(render.calledWith(inputStylus)).to.eq(true);
+      });
+    });
   });
 })
