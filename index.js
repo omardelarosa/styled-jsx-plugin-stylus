c3c517c1571adbed6d44d81dd83cfc7bf5f5c061
Omar Delarosa <thedelarosa@gmail.com>
2017-11-01 13:01:22 -0400

    0.0.3: using a more stylus-friendly placeholder

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 module.exports = (css, settings, stylus = require('stylus')) => {
   const cssWithPlaceholders = css
-    .replace(/\:\s*%%styled-jsx-placeholder-(\d+)%%/g, (_, id) =>
-      `: styled-jsx-placeholder-${id}()`
+    .replace(/\:\s*$styled-jsx-placeholder-(\d+)%%/g, (_, id) =>
+      `: \$styled-jsx-placeholder-${id}()`
     )
     .replace(/%%styled-jsx-placeholder-(\d+)%%/g, (_, id) =>
       `/*%%styled-jsx-placeholder-${id}%%*/`
     )
 
-  const preprocessed = stylus.render(css.toString(), settings);
+  const preprocessed = stylus.render(cssWithPlaceholders.toString(), settings);
 
   return preprocessed
-    .replace(/\:\s*styled-jsx-placeholder-(\d+)\(\)/g, (_, id) =>
+    .replace(/\:\s*\$styled-jsx-placeholder-(\d+)\(\)/g, (_, id) =>
       `: %%styled-jsx-placeholder-${id}%%`
     )
     .replace(/\/\*%%styled-jsx-placeholder-(\d+)%%\*\//g, (_, id) =>
